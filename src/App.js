e617264ab91da9c9dfc3b57589feda388efa6387
Rohit Singh <rohiisingh8655@gmail.com>
2023-07-10 19:49:02 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TextPlain from "./TextPlain";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-center my-5">Text Converter</h1>
+      <TextPlain />
+    </>
   );
 }
 
