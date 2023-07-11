cb49fb1b87c03c3808c93473ac5cbf3d90a068a1
Rohit Singh <rohiisingh8655@gmail.com>
2023-07-11 10:08:32 +0530

    Added an additional component Navigation bar to the App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import "./App.css";
+import Navbar from "./Navbar";
 import TextPlain from "./TextPlain";
+import { useState } from "react";
 
 function App() {
+  const [mode, setDarkMode] = useState("light");
+
+  const toogleMode = () => {
+    if (mode === "light") {
+      setDarkMode("dark");
+      document.body.style.backgroundColor = "black";
+    } else {
+      setDarkMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
   return (
     <>
-      <h1 className="text-center my-5">Text Converter</h1>
-      <TextPlain />
+      <Navbar title="MY React APP" mode={mode} toogleMode={toogleMode} />
+      <h1
+        className={`text-center my-5 ${
+          mode === "light" ? "text-black" : "text-white"
+        }`}
+      >
+        Text Converter
+      </h1>
+      <TextPlain mode={mode} />
     </>
   );
 }
