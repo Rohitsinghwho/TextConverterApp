7ba293e99000558dda03d282c3058f6ccdf1ef25
Rohit Singh <rohiisingh8655@gmail.com>
2023-07-11 11:13:58 +0530

    Added alert for navigation and other functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import "./App.css";
+import ComponentAlert from "./ComponentAlert";
 import Navbar from "./Navbar";
+
 import TextPlain from "./TextPlain";
 import { useState } from "react";
 
 function App() {
   const [mode, setDarkMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
 
   const toogleMode = () => {
     if (mode === "light") {
       setDarkMode("dark");
       document.body.style.backgroundColor = "black";
+      showAlert("DarkMode has been enabled", "success");
     } else {
       setDarkMode("light");
       document.body.style.backgroundColor = "white";
+      showAlert("LightMode has been enabled", "success");
     }
   };
   return (
     <>
       <Navbar title="MY React APP" mode={mode} toogleMode={toogleMode} />
+      <ComponentAlert alert={alert} />
+
       <h1
         className={`text-center my-5 ${
           mode === "light" ? "text-black" : "text-white"
@@ -25,7 +43,7 @@ function App() {
       >
         Text Converter
       </h1>
-      <TextPlain mode={mode} />
+      <TextPlain mode={mode} showAlert={showAlert} />
     </>
   );
 }
